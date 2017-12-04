a8b7ca6380d550714a3224528135aee52231d646
etuder <etuder@stevens.edu>
2017-12-04 02:43:24 -0500

    faves updates

--- script.js
+++ script.js
@@ -123,3 +123,67 @@
 	//   }
 
 	// }
+function handleFormSubmit() {
+    if (window.event) window.event.preventDefault();
+	// Get values of inputs
+	// Pass values to addNewPost()
+    var username= document.getElementById("input-username").value ;
+    var caption= document.getElementById("input-caption").value ;
+    var picture= document.getElementById("input-picture").value ;
+
+    addNewPost(username,caption,fileLocations[picture]);
+}
+
+/**
+ * This function create the following div and append it to the #post-list element
+	<div class="post">
+		<span>{username}</span>
+		<img src="{img_src}" alt="{caption}">
+		<div id="post-overlay">
+			{caption}
+		</div>
+	</div>
+ * 
+ * Also, add a mouseover and mouseleave events to the post div for opacity 
+ * transitions in the post-overlay div
+ */
+function addNewPost(category, acitivity){       
+    var node = document.createElement("DIV");
+    var node2 = document.createElement("DIV");
+    var span= document.createElement("span");
+ node.className="category";
+    node2.id="activity";
+    node2.innerText=category;
+    span.innerText = activity;
+    node.appendChild(span);
+    node.appendChild(node);
+    node.appendChild(node2);
+    document.getElementById("category").appendChild(node);
+
+
+                          
+});
+                          
+
+function summarize() 
+{
+.fan-favorites.get
+
+.summary_wrapper.display
+  //Summarize all input entered from user and display in "summary-wrapper" div, which is by default hidden.
+}
+
+function reset_form() 
+{
+  //reset form
+}
+
+	// Create the parent post div
+	// Create a span for the user
+	// Create image element
+	// Create overlay element
+	// Add all child elements (order matters)
+	// Add event listeners to post
+	// Add post element to post list
+}
+//var node= document.createElement("post");
